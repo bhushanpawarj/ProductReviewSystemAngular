86c706a1c72623490f8d9f84e06e5505001ec363
bhushanpawarj <35757357+bhushanpawarj@users.noreply.github.com>
2019-04-20 21:23:37 -0700

    Completed product Detail data service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,21 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { routerNgProbeToken } from '@angular/router/src/router_module';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { routerNgProbeToken } from "@angular/router/src/router_module";
+import { Product } from "../Models/product";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
 })
 export class ProductDetailsComponent implements OnInit {
+  selectedProduct: Product;
+  private itemDoc: AngularFirestoreDocument<Product>;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private afs: AngularFirestore) {}
 
-  ProductId:string;
+  /*
+  this.afs
+        .collection("Products", ref =>
+          ref.where("uid", "==", "tXMFim0YcGpZX2mjgSkT")
+        )
+        .valueChanges()
+        .subscribe(queriedItems => {
+          console.log(queriedItems);
+        });
+  */
+  ProductId: string;
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.ProductId = params['ProductId']; 
+      this.ProductId = params["ProductId"];
+      this.itemDoc = this.afs.doc<Product>("Products/" + this.ProductId);
+      this.itemDoc.valueChanges().subscribe(pro => {
+        this.selectedProduct = pro;
+      });
     });
   }
-
 }
